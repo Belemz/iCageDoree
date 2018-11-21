cef9b73df671e6a13fa87c3c866ff18bf37fbca3
inescrv <inescrv@gmail.com>
2018-11-21 00:25:47 +0000

    defined and added remaining elements in the "ensures" of function readExpertsFile.

--- cyberConcGroup18/filesReading.py
+++ cyberConcGroup18/filesReading.py
@@ -35,17 +35,34 @@ def readExpertsFile(fileName):
     Converts a given file listing experts into a collection
     Requires: fileName is str, the name of a .txt file listing experts,
     following the format specified in the project.
-    Ensures: list whose first element is ... <to complete>
+    Ensures: list whose first element is the name of the expert,
+        second is their geographic area of work,
+        third is their specialty,
+        fourth is their review rate,
+        fifth is their hourly pay,
+        sixth is the end date of their last order,
+        seventh is starting time of next available slot,
+        eighth is the accumulated amount (in €) for the completed work
     """
     outputList = []
     
     outputList.append(readHeader(fileName))
+    print(outputList)
     
     fileIn = open(fileName, 'r')
 
-    # ... <to complete>
+    expertsList = []
+    counter = 0
+    for line in fileIn.readlines ():
+        if counter >= 7:
+            line.replace ("\n", "")
+            expertsList.append (line.split (","))
+        counter += 1
 
-    return (outputList)
+    inFile.close()
+
+    return (expertsList)
+
 
 
 def readClientsFile(fileName):
